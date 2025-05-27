b34c9e77849a6da7027bf7e244651decffcd3d11
Livia Carvalho <livia.carvalho.lb@gmail.com>
2025-05-26 20:56:42 -0400

    initial setup

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+// entry point of ktor backend
+fun Application.module() {
+
+}
